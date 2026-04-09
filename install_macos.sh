febd0aa9c34176336b60c49cc5c5fa2dfe3f9714
aokpc <181478235+aokpc@users.noreply.github.com>
2026-04-09 09:23:05 +0900

    update

--- install_macos.sh
+++ install_macos.sh
@@ -4,6 +4,7 @@ echo "installing arduino extension"
 cp ./extension/uimaker-ext-0.0.1.vsix ~/.arduinoIDE/plugins/
 
 echo "installing arduino extension (recommended-extensions)"
+unzip ./recommended-extensions.zip
 cp -r ./recommended-extensions/* ~/.arduinoIDE/deployedPlugins/
 
 echo "done."
